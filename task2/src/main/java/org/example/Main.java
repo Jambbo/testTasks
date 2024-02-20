bcb27ab85c58e37c98c8a522c08857e0660e5734
Hlib Chibichian <chibgleb99@gmail.com>
2024-02-20 19:50:25 +0000

    task commit

--- task2/src/main/java/org/example/Main.java
+++ task2/src/main/java/org/example/Main.java
@@ -24,49 +24,50 @@ public class Main {
         }
     }
 
-        private static double[] readDataFromFile(String fileName) {
-            double[] data = new double[3];
-            try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-                String line = reader.readLine();
+    private static double[] readDataFromFile(String fileName) {
+        double[] data = new double[3];
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line = reader.readLine();
+            String[] parts = line.split(" ");
+            for (int i = 0; i < 3; i++) {
+                data[i] = Double.parseDouble(parts[i]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    private static double[][] readPointsFromFile(String fileName) {
+        double[][] points = new double[100][2];
+        int index = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(" ");
-                for (int i = 0; i < 3; i++) {
-                    data[i] = Double.parseDouble(parts[i]);
+                for (int i = 0; i < 2; i++) {
+                    points[index][i] = Double.parseDouble(parts[i]);
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+                index++;
             }
-            return data;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return points;
+    }
 
-        private static double[][] readPointsFromFile(String fileName) {
-            double[][] points = new double[100][2];
-            int index = 0;
-            try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    String[] parts = line.split(" ");
-                    for (int i = 0; i < 2; i++) {
-                        points[index][i] = Double.parseDouble(parts[i]);
-                    }
-                    index++;
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return points;
-        }
-        private static double calculateDistance(double x1, double y1, double x2, double y2) {
-            return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        }
+    private static double calculateDistance(double x1, double y1, double x2, double y2) {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
 
-        private static int getResult(double distance, double radius) {
-            if (distance == radius) {
-                return 0; // т. лежит на окружности
-            } else if (distance < radius) {
-                return 1; // т. внутри окружности
-            } else {
-                return 2; // т. снаружи окружности
-            }
+    private static int getResult(double distance, double radius) {
+        if (distance == radius) {
+            return 0; // т. лежит на окружности
+        } else if (distance < radius) {
+            return 1; // т. внутри окружности
+        } else {
+            return 2; // т. снаружи окружности
         }
+    }
 
 }
